6bdcf4c0d6d5f3096c2a5de268087406b00a2d57
Jessica Lin <JLin@ucvts.tec.nj.us>
2019-10-28 13:08:26 -0400

    exercise 1

--- src/change.js
+++ src/change.js
@@ -3,12 +3,32 @@ const readlineSync = require("readline-sync");
 const MIN = 0.01;
 const MAX = 0.99;
 
-const amount = Number(readlineSync.question("\nEnter a dollar amount: "));
+let amount = Number(readlineSync.question("\nEnter a dollar amount: "));
 
 if (Number.isNaN(amount)) {
-    // print your error message here
+    console.log("\nInvalid.");
 } else if (amount < MIN || amount > MAX) {
-    // print your error message here
+    console.log("\nInvalid.")
 } else {
-    // write your code for making change here
+    const QUARTER = 0.25 ;
+    const DIME = 0.10 ;
+    const NICKEL = 0.05 ;
+    const PENNY = 0.01 ;
+    let amountOfQuarter = 0 ;
+    let amountOfDime = 0 ;
+    let amountOfNickel = 0 ;
+    let amountOfPenny = 0 ;
+      while (amount >= QUARTER) {
+        amount = amount - QUARTER ;
+        amountOfQuarter ++ ;
+      } while (amount >= DIME) {
+        amount = amount - DIME ;
+        amountOfDime ++ ;
+      } while (amount >= NICKEL) {
+        amount = amount - NICKEL ;
+        amountOfNickel ++ ;
+      } while (amount >= PENNY) {
+        amount = amount - PENNY ;
+        amountOfPenny ++ ;
+      } console.log("\n" + amountOfQuarter + " quarters, " + amountOfDime + " dimes, " + amountOfNickel + " nickels, and " + amountOfPenny + " pennies.");
 }
