dbe224589f0ed5b4c8f08ecbddbffe58e270b859
Jessica Lin <JLin@ucvts.tec.nj.us>
2019-10-30 13:20:36 -0400

    exercise 2

--- src/sign.js
+++ src/sign.js
@@ -1,3 +1,18 @@
 const readlineSync = require("readline-sync");
 
 const integer = readlineSync.question("\nEnter a number: ");
+
+const MIN = Number.MIN_SAFE_INTEGER ;
+const MAX = Number.MAX_SAFE_INTEGER ;
+
+if (Number.isNaN(integer)) {
+    console.log("\nInvalid.")
+} else if (integer <= MIN || integer >= MAX) {
+  console.log("\nInvalid.")
+} else if (integer == 0) {
+  console.log("\nZero.")
+} else if (integer > +0) {
+    console.log("\nPositive.")
+} else {
+  console.log("\nNegative.")
+}
