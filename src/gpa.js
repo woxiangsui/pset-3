7e5c0d6c85137566b9e62cdebf7858b30a6c7ae6
Jessica Lin <JLin@ucvts.tec.nj.us>
2019-11-05 13:06:31 -0500

    changed something

--- src/gpa.js
+++ src/gpa.js
@@ -2,13 +2,26 @@ const readlineSync = require("readline-sync");
 
 const grade = readlineSync.question("\nEnter a letter grade: ");
 
-const MIN = 4 ;
-const MAX = 0 ;
-
 if (Number.isNaN(grade)) {
   console.log("\nInvalid.");
-} else if (grade = B) {
+} else if (grade == "A" || grade == "A+") {
+    console.log("\nYour GPA is 4.00.");
+} else if (grade == "A-") {
+    console.log("\nYour GPA is 3.67.");
+} else if (grade == "B-") {
     console.log("\nYour GPA is 3.00.");
+} else if (grade == "B+") {
+    console.log("\nYour GPA is 3.33.");
+} else if (grade == "C+") {
+    console.log("\nYour GPA is 2.33.");
+} else if (grade == "C-") {
+    console.log("\nYour GPA is 2.00.");
+} else if (grade == "D+") {
+    console.log("\nYour GPA is 1.33.");
+} else if (grade == "D-") {
+    console.log("\nYour GPA is 1.00.");
+} else if (grade == "F") {
+    console.log("\nYour GPA is 0.00.");
 } else {
   console.log("\nInvalid.");
 }
