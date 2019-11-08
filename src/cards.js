4f5814709266606ae49a8c5bbb665cb3a0b4155b
unknown <jlin@ucvts.org>
2019-11-08 11:37:39 -0500

    exercise 8

---
+++ src/cards.js
@@ -0,0 +1,111 @@
+const readlineSync = require("readline-sync");
+
+const card = readlineSync.question("\nEnter a playing card: ");
+
+if (card == "2C") {
+  console.log("\nTwo of Clubs.");
+} else if (card == "2D") {
+    console.log("\nTwo of Diamonds.");
+} else if (card == "2H") {
+    console.log("\nTwo of Hearts.");
+} else if (card == "2S") {
+    console.log("\nTwo of Spades");
+} else if (card == "3C") {
+  console.log("\nThree of Clubs.");
+} else if (card == "3D") {
+    console.log("\nThree of Diamonds.");
+} else if (card == "3H") {
+    console.log("\nThree of Hearts.");
+} else if (card == "3S") {
+    console.log("\nThree of Spades");
+} else if (card == "4C") {
+  console.log("\nFour of Clubs.");
+} else if (card == "4D") {
+    console.log("\nFour of Diamonds.");
+} else if (card == "4H") {
+    console.log("\nFour of Hearts.");
+} else if (card == "4S") {
+    console.log("\nFour of Spades");
+} else if (card == "5C") {
+  console.log("\nFive of Clubs.");
+} else if (card == "5D") {
+    console.log("\nFive of Diamonds.");
+} else if (card == "5H") {
+    console.log("\nFive of Hearts.");
+} else if (card == "5S") {
+    console.log("\nFive of Spades");
+} else if (card == "6C") {
+  console.log("\nSix of Clubs.");
+} else if (card == "6D") {
+    console.log("\nSix of Diamonds.");
+} else if (card == "6H") {
+    console.log("\nSix of Hearts.");
+} else if (card == "6S") {
+    console.log("\nSix of Spades");
+} else if (card == "7C") {
+  console.log("\nSeven of Clubs.");
+} else if (card == "7D") {
+    console.log("\nSeven of Diamonds.");
+} else if (card == "7H") {
+    console.log("\nSeven of Hearts.");
+} else if (card == "7S") {
+    console.log("\nSeven of Spades");
+} else if (card == "8C") {
+  console.log("\nEight of Clubs.");
+} else if (card == "8D") {
+    console.log("\nEight of Diamonds.");
+} else if (card == "8H") {
+    console.log("\nEight of Hearts.");
+} else if (card == "8S") {
+    console.log("\nEight of Spades");
+} else if (card == "9C") {
+  console.log("\nNine of Clubs.");
+} else if (card == "9D") {
+    console.log("\nNine of Diamonds.");
+} else if (card == "9H") {
+    console.log("\nNine of Hearts.");
+} else if (card == "9S") {
+    console.log("\nNine of Spades");
+} else if (card == "TC") {
+  console.log("\nTen of Clubs.");
+} else if (card == "TD") {
+    console.log("\nTen of Diamonds.");
+} else if (card == "TH") {
+    console.log("\nTen of Hearts.");
+} else if (card == "TS") {
+    console.log("\nTen of Spades");
+} else if (card == "JC") {
+  console.log("\nJack of Clubs.");
+} else if (card == "JD") {
+    console.log("\nJack of Diamonds.");
+} else if (card == "JH") {
+    console.log("\nJack of Hearts.");
+} else if (card == "JS") {
+    console.log("\nJack of Spades");
+} else if (card == "QC") {
+  console.log("\nQueen of Clubs.");
+} else if (card == "QD") {
+    console.log("\nQueen of Diamonds.");
+} else if (card == "QH") {
+    console.log("\nQueen of Hearts.");
+} else if (card == "QS") {
+    console.log("\nQueen of Spades");
+} else if (card == "KC") {
+  console.log("\nKing of Clubs.");
+} else if (card == "KD") {
+    console.log("\nKing of Diamonds.");
+} else if (card == "KH") {
+    console.log("\nKing of Hearts.");
+} else if (card == "KS") {
+    console.log("\nKing of Spades");
+} else if (card == "AC") {
+  console.log("\nAce of Clubs.");
+} else if (card == "AD") {
+    console.log("\nAce of Diamonds.");
+} else if (card == "AH") {
+    console.log("\nAce of Hearts.");
+} else if (card == "AS") {
+    console.log("\nAce of Spades");
+} else {
+    console.log("\nInvalid.");
+}
