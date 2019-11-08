8d769e472b445427bf08f34b5165071776a6dba5
unknown <jlin@ucvts.org>
2019-11-08 11:13:59 -0500

    exercise 7

--- src/years.js
+++ src/years.js
@@ -5,23 +5,26 @@ const year = readlineSync.question("\nEnter a year: ");
 const MIN = 0 ;
 const MAX = Number.MAX_SAFE_INTEGER ;
 
-if (Number.isNaN(year)) {
+if (Number.isNaN(year) == true) {
     console.log("\nInvalid.");
 } else if (year < MIN || year > MAX) {
     console.log("\nInvalid.");
+} else if (Number.isInteger(year)) {
+    console.log("\nInvalid.");
 } else if (year % 4 == 0) {
-    if (year % 400 == 0) {
-        console.log("\n" + year + " is a leap year.");
-      } else {
-          console.log("\n" + year + " is a leap year.");
-      }
-} else if (year % 100 == 0) {
-    if (year % 400 == 0) {
+    if (year % 100 != 0) {
       console.log("\n" + year + " is a leap year.");
     } else {
-      console.log("\n" + year + " is not a leap year.");
+        console.log("\n" + year + " is not a leap year.");
     }
-}
-else {
+} else if (year % 100 == 0) {
+  if (year % 400 == 0) {
+    console.log("\n" + year + " is a leap year.");
+  } else {
+    console.log("\n" + year + " is not a leap year.");
+  }
+} else if (year > MIN || year < MAX) {
+console.log("\n" + year + " is not a leap year.");
+} else {
     console.log("\nInvalid.");
 }
