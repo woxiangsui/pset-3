1dd466871864ae07cbf0da0fd3fc29d61ae32820
unknown <jlin@ucvts.org>
2019-11-07 22:00:41 -0500

    exercise 6

--- src/grades.js
+++ src/grades.js
@@ -2,21 +2,25 @@ const readlineSync = require("readline-sync");
 
 const grade = readlineSync.question("\nEnter a grade: ");
 
- const MAXA = 100 ;
- const MINA = 90 ;
+const MAX = 100 ;
+const B = 90 ;
+const C = 80 ;
+const D = 70 ;
+const F = 60 ;
+const MIN = 0 ;
 
 
 if (Number.isNaN(grade)) {
   console.log("\nInvalid.");
-} else if (grade <= MINA && grade >= MAXA) {
+} else if (grade >= B && grade <= MAX) {
     console.log("\nYou received an A.");
-} else if (grade <= 89 && grade >= 80) {
+} else if (grade < B && grade >= C) {
     console.log("\nYou received a B.");
-} else if (grade <= 79 && grade >= 70) {
+} else if (grade < C  && grade >= D) {
     console.log("\nYou received a C.");
-} else if (grade <= 69 && grade >= 60) {
+} else if (grade < D && grade >= F) {
     console.log("\nYou received a D.");
-} else if (grade <= 59 && grade >= 0) {
+} else if (grade < F && grade >= MIN) {
     console.log("\nYou received a F.");
 } else {
     console.log("\nInvalid.")
