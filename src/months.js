29f3c75477e0654ca9474acde93b87fc4f8c2e47
unknown <jlin@ucvts.org>
2019-11-07 22:24:13 -0500

    fixed spacing

--- src/months.js
+++ src/months.js
@@ -8,15 +8,15 @@ if (month == "jan" || month == "January" || month == "january" ||
     month == "jul" || month == "July" || month == "july" ||
     month == "aug" || month == "August" || month == "august" ||
     month == "oct" || month == "October" || month == "october" ||
-    month == "dec" || month == "December" || month == "december"){
-    console.log("\n31 days.");
+    month == "dec" || month == "December" || month == "december") {
+      console.log("\n31 days.");
 } else if (month == "feb" || month == "February" || month == "february") {
     console.log("\n28 or 29 days.");
 } else if (month == "apr" || month == "April" || month == "april" ||
-          month == "jun" || month == "June" || month == "june" ||
-          month == "sep" || month == "September" || month == "september" ||
-          month == "nov" || month == "November" || month == "november") {
-    console.log("\n30 days.");
+      month == "jun" || month == "June" || month == "june" ||
+      month == "sep" || month == "September" || month == "september" ||
+      month == "nov" || month == "November" || month == "november") {
+        console.log("\n30 days.");
 } else {
     console.log("\nInvalid.");
 }
