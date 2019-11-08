a60cc207ff148ae4ed30c2033b80a9b58746e644
unknown <jlin@ucvts.org>
2019-11-08 12:04:15 -0500

    exercise 10

---
+++ src/temperature.js
@@ -0,0 +1,45 @@
+const readlineSync = require("readline-sync");
+
+const temp = readlineSync.question("\nEnter a temperature: ");
+const scale = readlineSync.question("Enter a scale: ");
+
+const MAX = Number.MAX_SAFE_INTEGER;
+const MIN = Number.MIN_SAFE_INTEGER;
+
+if (Number.isNaN(temp) || Number.isNaN(scale)) {
+  console.log("\nInvalid.");
+} else if (temp < MIN || temp > MAX || scale < MIN || scale > MAX) {
+    console.log("\nInvalid.");
+} else if (scale == "F") {
+    if (temp >= MIN && temp <= 32) {
+      console.log("\nSolid.");
+    } else if (temp > 32 && temp < 212) {
+        console.log("\nLiquid.");
+    } else if (temp >= 212 && temp <= MAX) {
+        console.log("\nGas.");
+    } else {
+        console.log("\nInvalid.");
+    }
+} else if (scale == "C") {
+    if (temp >= MIN && temp <= 0) {
+      console.log("\nSolid.");
+    } else if (temp > 0 && temp < 100) {
+        console.log("\nLiquid.");
+    } else if (temp >= 100 && temp <= MAX) {
+        console.log("\nGas.");
+    } else {
+        console.log("\nInvalid.");
+    }
+} else if (scale == "K") {
+    if (temp >= 0 && temp <= 273.15) {
+      console.log("\nSolid.");
+    } else if (temp > 273.15 && temp < 373.15) {
+        console.log("\nLiquid.");
+    } else if (temp >= 373.15 && temp <= MAX) {
+        console.log("\nGas.");
+    } else {
+        console.log("\nInvalid.");
+    }
+} else {
+    console.log("\nInvalid.");
+}
